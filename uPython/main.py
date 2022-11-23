c31564fe1154158e9134dd837b03a21a98c58c56
ignasp <ignas.plauska@ktu.lt>
2022-11-23 12:44:46 +0200

    Update main.py

--- uPython/main.py
+++ uPython/main.py
@@ -9,6 +9,15 @@ def run():
     Print_header(globalPrintType)
 
     t = Tester()
+    
+    from a_tests import t_OVERHEAD_run as runner
+    t.TestName =    "OVERHEAD"
+    t.TestLengths = [0, 16, 32, 64, 128, 256, 512, 1024]
+    t.Niterations = globalIterationN
+    t.PrintType =   globalPrintType
+    t.RunFn =       runner
+    t.Run()
+    del runner
 
     from a_tests import t_CRC32_run as runner
     t.TestName =    "CRC32"
